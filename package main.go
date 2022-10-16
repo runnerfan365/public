dc618d91313a804f190ce5cf8945cfcf439a67b0
runnerfan365 <50909744+runnerfan365@users.noreply.github.com>
2022-10-16 23:09:15 +0300

    Add files via upload

---
+++ package main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s sortRunes) Len() int {
+	return len(s)
+}
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
+
+func rollUp(s string) string {
+	var temp_str, res_str string
+	var ch_count int = 0
+	var ch rune
+	temp_str = SortString(s)
+	for _, tch := range temp_str {
+		if ch != tch {
+			if ch_count > 0 {
+				res_str = res_str + strconv.Itoa(ch_count)
+				ch_count = 0
+			}
+			ch = tch
+			res_str = res_str + string(tch)
+			ch_count = ch_count + 1
+		} else {
+			ch_count = ch_count + 1
+		}
+	}
+	res_str = res_str + strconv.Itoa(ch_count)
+	return res_str
+}
+
+func main() {
+	w1 := "bcadaabddd"
+	w2 := SortString(w1)
+	fmt.Println(w1)
+	fmt.Println(w2)
+	fmt.Println(rollUp(w1))
+}
